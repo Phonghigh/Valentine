6d2430f032bf0c55cff616abb1eb70d7be25283d
Phonghigh <126806274+Phonghigh@users.noreply.github.com>
2025-02-14 00:47:35 +0700

    Update việt hóa

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Chắc chưa?",
+    "Thật luôn :((",
+    "Hông đùa mà đúng hôg?",
+    "Hoooooi màaaaaaa",
+    "Suy nghĩ lại ikkkkkkk!",
+    "Trúc Trúc hoogn đi, Fog buồn đó T-T",
+    "Buồn luônnnnn...",
+    "Rất rất buồn luônnnnn...",
+    "Ok fine, Kìn chán nà...",
+    "Giỡn hoiffoiiiii, đồng ý chứ gìiiiii! ❤️"
 ];
 
 let messageIndex = 0;
